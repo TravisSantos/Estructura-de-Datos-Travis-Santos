00e55652128b953f4cf58816bf17bbd63b612795
TravisSantos <travis_sant07@hotmail.com>
2025-09-09 14:15:23 -0700

    Practica 1 c++

---
+++ Practica 1 C++/Practica 1 C++.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numeros[] = { 1, 2, 3, 4, 5, 6 };
+
+    // Imprimir el array
+    cout << numeros[0] << " "
+        << numeros[1] << " "
+        << numeros[2] << " "
+        << numeros[3] << " "
+        << numeros[4] << " "
+        << numeros[5] << endl;
+
+    return 0;
+}
