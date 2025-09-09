797d827e4fe14bf379c8bfffa41c50d77742cc18
TravisSantos <travis_sant07@hotmail.com>
2025-09-09 14:08:14 -0700

    Practica 1 de c# iniciada

---
+++ Practica 1 C#/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declarar el array
+        int[] numeros = { 1, 2, 3, 4, 5, 6 };
+
+        // Imprimir todo el array 
+        Console.WriteLine(string.Join(", ", numeros));
+    }
+}
