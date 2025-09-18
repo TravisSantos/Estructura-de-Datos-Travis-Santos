908d326b8547435171ced18a26d8979cf2a42d77
TravisSantos <travis_sant07@hotmail.com>
2025-09-18 14:58:59 -0700

    Bubble sort java

---
+++ Bubble sort.java
@@ -0,0 +1,32 @@
+class Main {
+    // Método que aplica Bubble Sort al arreglo
+    static void bubbleSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Intercambiar arr[j] y arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Arreglo original:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        bubbleSort(arr);
+
+        System.out.println("\n\nArreglo ordenado:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
